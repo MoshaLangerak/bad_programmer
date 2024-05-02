1e822f9cf1ec45d8cf65bdc9ce4fb7b32045d2dc
Mosha Langerak <82883699+MoshaLangerak@users.noreply.github.com>
2024-05-02 14:32:04 +0200

    Added responses

--- bad_programmer.py
+++ bad_programmer.py
@@ -34,13 +34,34 @@ def display_ui():
     st.title('Meet the bad programmer!')
     st.write("This is a bad programmer. He writes bad code. However, he is trying to improve. Can you help him?")
 
+def get_and_process_prompt():
+    if st.session_state.messages[-1]['role'] == "user":
+        # Generate a response
+        with st.chat_message("Bad Programmer", avatar="🤖"):
+            response = "Response"
+            st.write_stream(stream_chat_message(response))
+        st.session_state.messages.append({"role": "Bad Programmer", "avatar": "🤖", "content": response})
+        
+    if st.session_state.chat_finished:
+        st.button('New problem please!', on_click=restart_chat, key="new_problem")
+        st.chat_input(disabled=True)
+    elif prompt := st.chat_input("Type your message here..."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.rerun()
+
+def restart_chat():
+    st.session_state.messages = []
+    st.session_state.chat_finished = False
+    st.rerun()
+    
 def main():
     get_replicate_api_token()
     display_ui()
 
-    # Initialize the chatbot
+    # Initialize the session state
     if "messages" not in st.session_state:
         st.session_state.messages = []
+        st.session_state.chat_finished = False
 
     # if there are no messages, display the initial message
     if not st.session_state.messages:
@@ -57,11 +78,7 @@ def main():
                 with st.chat_message(message['role'], avatar=message.get("avatar", None)):
                     st.write(message['content'])
         
-    if prompt := st.chat_input("Type your message here..."):
-        with st.chat_message("user"):
-            st.write(prompt)
-
-        st.session_state.messages.append({"role": "user", "content": prompt})
+    get_and_process_prompt()
 
 if __name__ == "__main__":
     main()
